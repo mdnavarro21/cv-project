6b2d58996e765755b835b8a986415bd1469a2198
Matthew Dylan <dylannavarro65@gmail.com>
2022-06-24 19:39:59 -0400

    more styling

--- src/components/main-components/Overview.js
+++ src/components/main-components/Overview.js
@@ -14,9 +14,16 @@ class Overview extends Component {
                 </section>
                 <section id = ''>
                     <h3>{education.schoolName}</h3>
+                    <h4>{education.degreeName}</h4>
+                    <h4>{education.programName}</h4>
+                    <h4>{education.dateOfStudy}</h4>
                 </section>
                 <section>
                     <h3>{experience.companyName}</h3>
+                    <h4>{experience.positionTitle}</h4>
+                    <p>{experience.mainTasks}</p>
+                    <h4>{experience.startDate}</h4>
+                    <h4>{experience.endDate}</h4>
                 </section>
             </div>
         )
